afa356c6a9ed2bb529cf35549d1420c1f5b20ade
jaylenmc <jaylenmc05@gmail.com>
2025-04-01 03:27:01 -0500

    Just finished task api's

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
-    # path('tasks/', views.userTasks, name='userTasks'),
-    # path('tasks/delete/<int:task_id>/',views.deleteTask, name='deleteTask' )
+    path('newtask/', views.newTask),
+    path('edit/', views.editTask),
+    path('delete/<int:task_id>', views.deleteTask)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,25 +1,54 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from rest_framework.response import Response
 from django.conf import settings
-from .models import Task, AppUser
+from .models import Task
+from authentication.models import AppUser
 from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view
+from .serializer import taskSerializer
+from rest_framework import status
 
 @login_required
 @api_view(['POST'])
-def userTasks(request):
-    user = request.data.get('email')
-    list_items = Task.objects.all().filter(user=user)
-    serilizer = taskSerializer(list_items, many=True)
-    context = {
-        'form': form,
-        'tasks': serilizer.data,
-    }
+def newTask(request):
+    task_user = AppUser.objects.filter(email=request.data.get('email')).first()
+    task_title = request.data.get('title')
+    task_description = request.data.get('description', 'No description provided')
+    new_task = Task.objects.create(
+        user = task_user,
+        title = task_title,
+        description = task_description
+    )
+    new_task.save()
 
-    return render(request, 'tasks/todo.html', context=context)
+    serilizer = taskSerializer(new_task)
 
+    return Response(serilizer.data, status=200)
+
+@login_required
+@api_view(['POST'])
+def editTask(request):
+    task_user = AppUser.objects.filter(email=request.data.get('email')).first()
+    new_title = request.data.get('title')
+    new_description = request.data.get('description')
+    task_id = request.data.get('id')
+
+    updating_task = task_user.tasks.filter(id=task_id).first()
+    updating_task.title = new_title
+    updating_task.description = new_description
+    updating_task.save()
+
+    serialized = taskSerializer(updating_task)
+
+    return Response(serialized.data)
+
+@login_required
+@api_view(['DELETE'])
 def deleteTask(request, task_id):
-    if request.method == 'POST':
-        task = get_object_or_404(Task, id=task_id)
-        task.delete()
+    user = AppUser.objects.filter(email=request.user).first()
+    task = user.tasks.filter(id=task_id).first()
+    if not task:
+        return Response({"Error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    task.delete()
 
-    return redirect('/tasks/')
+    return Response(status=status.HTTP_204_NO_CONTENT)
